4a329a28fe05d725d594f8a74550bbc3526c469e
Felipe Urbanski <felipeurbansk@gmail.com>
2020-05-11 01:30:01 -0300

    Change connection mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri_mongo = 'mongodb+srv://admin:Ludovico@cluster0-rzcmf.gcp.mongodb.net/authid?retryWrites=true&w=majority';
+const uri_mongo = 'mongodb+srv://admin:Ludovico@cluster0-rzcmf.gcp.mongodb.net/authid?retryWrites=true&w=majority&ssl=true';
 
 mongoose.connect( uri_mongo, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true} );
 mongoose.Promise = global.Promise;
